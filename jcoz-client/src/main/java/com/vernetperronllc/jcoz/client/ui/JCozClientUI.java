09a96367e93fd97aaf88cce89277091fa05fac03
David Vernet <Dcvernet@gmail.com>
2016-07-31 17:08:18 -0400

    Terminate client process on window close

--- jcoz-client/src/main/java/com/vernetperronllc/jcoz/client/ui/JCozClientUI.java
+++ jcoz-client/src/main/java/com/vernetperronllc/jcoz/client/ui/JCozClientUI.java
@@ -20,30 +20,11 @@
  */
 package com.vernetperronllc.jcoz.client.ui;
 
-import com.sun.tools.attach.VirtualMachine;
-import com.sun.tools.attach.VirtualMachineDescriptor;
-import com.vernetperronllc.jcoz.service.JCozException;
-import com.vernetperronllc.jcoz.service.VirtualMachineConnectionException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 
 /**
@@ -56,10 +37,15 @@ import javafx.stage.Stage;
 public class JCozClientUI extends Application {
     
     @Override
-    public void start(Stage primaryStage) {        
+    public void start(Stage primaryStage) {
         primaryStage.setTitle("JCoz Client");
         primaryStage.setScene(PickProcessScene.getPickProcessScene(primaryStage));
         primaryStage.show();
+    }
+    
+    @Override
+    public void stop() {
+		System.exit(0);
     }
 
     /**
