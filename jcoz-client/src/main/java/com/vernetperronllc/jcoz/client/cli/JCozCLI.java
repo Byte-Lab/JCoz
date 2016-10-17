b454fed6d5f755d698c63e11340f6fada44277a2
David Vernet <Dcvernet@gmail.com>
2016-10-17 00:21:20 -0400

    Update CLI to allow remote profiling

--- jcoz-client/src/main/java/com/vernetperronllc/jcoz/client/cli/JCozCLI.java
+++ jcoz-client/src/main/java/com/vernetperronllc/jcoz/client/cli/JCozCLI.java
@@ -21,6 +21,7 @@
 package com.vernetperronllc.jcoz.client.cli;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -31,7 +32,9 @@ import org.apache.commons.cli.ParseException;
 
 import com.sun.tools.attach.VirtualMachine;
 import com.sun.tools.attach.VirtualMachineDescriptor;
+import com.vernetperronllc.jcoz.client.ui.VisualizeProfileScene;
 import com.vernetperronllc.jcoz.profile.Experiment;
+import com.vernetperronllc.jcoz.profile.Profile;
 import com.vernetperronllc.jcoz.service.JCozException;
 import com.vernetperronllc.jcoz.service.VirtualMachineConnectionException;
 
@@ -61,63 +64,90 @@ public class JCozCLI {
 		scopeOption.setRequired(true);
 		ops.addOption(scopeOption);
 		
+		Option remoteHostOption = new Option("h", "host", true, "Remote hostname containing JVM process to profile");
+		remoteHostOption.setRequired(false);
+		ops.addOption(remoteHostOption);
+		
 		CommandLineParser parser = new DefaultParser();
 		CommandLine cl = parser.parse(ops, args);
 		String ppClass = cl.getOptionValue('c');
 		String scopePkg = cl.getOptionValue('s');
-		int ppLineNo;
-		int pid;
+		int ppLineNo = -1;
+		int pid = -1;
 		try{
 			ppLineNo = Integer.parseInt(cl.getOptionValue('l'));
 		}catch(NumberFormatException e){
 			System.err.println("Invalid Line Number : "+ cl.getOptionValue('l'));
-			return;
+			System.exit(-1);
 		}
 		try{
 			pid = Integer.parseInt(cl.getOptionValue('p'));
 		}catch(NumberFormatException e){
 			System.err.println("Invalid pid : "+ cl.getOptionValue('l'));
-			return;
-		}
-		VirtualMachineDescriptor descriptor = null;
-		for(VirtualMachineDescriptor vmDesc : VirtualMachine.list()){
-			if (vmDesc.id().equals(Integer.toString(pid))){
-				descriptor = vmDesc;
-				break;
-			}
-		}
-		if(descriptor == null){
-			System.err.println("Could not find java process with pid : "+ pid);
-			return;
+			System.exit(-1);
 		}
 		
-		final LocalProcessWrapper wrapper = new LocalProcessWrapper(descriptor);
-		//catch SIGINT and end profiling
-		Runtime.getRuntime().addShutdownHook(new Thread()
-        {
-            @Override
-            public void run()
-            {
-                try {
-					wrapper.endProfiling();
-					System.exit(0);
-				} catch (JCozException e) {
-					// we are dying, do nothing
+		String remoteHost = cl.getOptionValue('h');
+		boolean isRemote = remoteHost != null && !remoteHost.equals("");
+		if (isRemote) {
+			try {
+				Profile profile = new Profile(remoteHost + "-" + scopePkg, null);
+				final RemoteServiceWrapper remoteService = new RemoteServiceWrapper(remoteHost);
+				TargetProcessInterface profiledClient = remoteService.attachToProcess(pid);
+				profiledClient.setProgressPoint(ppClass, ppLineNo);
+				profiledClient.setScope(scopePkg);
+				profiledClient.startProfiling();
+
+				while (true) {
+					// Sleep for 2 seconds
+					Thread.sleep(2000);
+					
+					List<Experiment> experiments = profiledClient.getProfilerOutput();
+					profile.addExperiments(experiments);
 				}
-            }
-        });
-		wrapper.setProgressPoint(ppClass, ppLineNo);
-		wrapper.setScope(scopePkg);
-		wrapper.startProfiling();
-		while(true){
-			for(Experiment e : wrapper.getProfilerOutput()){
-				System.out.println(e.toString());
+			} catch (JCozException e) {
+				System.err.println("Unable to connect to target process.");
+				e.printStackTrace();
 			}
-			Thread.sleep(1000);
+		} else {
+			VirtualMachineDescriptor descriptor = null;
+			for(VirtualMachineDescriptor vmDesc : VirtualMachine.list()){
+				if (vmDesc.id().equals(Integer.toString(pid))){
+					descriptor = vmDesc;
+					break;
+				}
+			}
+			if(descriptor == null){
+				System.err.println("Could not find java process with pid : "+ pid);
+				return;
+			}
+			
+			final LocalProcessWrapper wrapper = new LocalProcessWrapper(descriptor);
+			//catch SIGINT and end profiling
+			Runtime.getRuntime().addShutdownHook(new Thread()
+	        {
+	            @Override
+	            public void run()
+	            {
+	                try {
+						wrapper.endProfiling();
+						System.exit(0);
+					} catch (JCozException e) {
+						// we are dying, do nothing
+					}
+	            }
+	        });
+			wrapper.setProgressPoint(ppClass, ppLineNo);
+			wrapper.setScope(scopePkg);
+			wrapper.startProfiling();
+			while(true){
+				for(Experiment e : wrapper.getProfilerOutput()){
+					System.out.println(e.toString());
+				}
+				Thread.sleep(1000);
+			}
+			
+			// search through and 
 		}
-		
-		// search through and 
-		
-		
 	}
 }
