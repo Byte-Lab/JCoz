723d8af3a21a58b2425ce82c738c8492f60c98a0
David Vernet <Dcvernet@gmail.com>
2016-10-26 23:11:45 -0400

    Add logProgressPoint hit method to the JCoz profiler.
    * Adds static reference in the CLI to the target process interface. This
    can then be used by the targeted progress point line to access the
    interface for logging a progress point hit.
    * The code for performing the bytecode translation will be added in a
    future commit.

--- jcoz-client/src/main/java/com/vernetperronllc/jcoz/agent/JCozProfilerMBean.java
+++ jcoz-client/src/main/java/com/vernetperronllc/jcoz/agent/JCozProfilerMBean.java
@@ -31,6 +31,8 @@ public interface JCozProfilerMBean {
 	
 	public int setProgressPoint(String className, int lineNo);
 	
+	public int logProgressPointHit();
+	
 	public int setScope(String scope);
 	
 	public byte[] getProfilerOutput() throws IOException;
