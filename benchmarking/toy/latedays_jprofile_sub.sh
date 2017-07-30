47bb9e3d1ef20100fc878c47a2fdf92362de2db4
David Vernet <Dcvernet@gmail.com>
2017-07-29 18:28:24 -0700

    Remove benchmarking suite

--- benchmarking/toy/latedays_jprofile_sub.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/bash
-
-echo "Running TestThreadSerial for JProfile"
-
-JCOZ_HOME="${HOME}/JCoz"
-JCOZ_TEST_CLASSES="${JCOZ_HOME}/jcoz-client/target/test-classes"
-
-java -cp ${JCOZ_TEST_CLASSES} test.TestThreadSerial
--- benchmarking/toy/latedays_xprof_sub.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/bash
-
-echo "Running TestThreadSerial with xprof"
-
-JCOZ_HOME="${HOME}/JCoz"
-JCOZ_TEST_CLASSES="${JCOZ_HOME}/jcoz-client/target/test-classes"
-
-java -cp ${JCOZ_TEST_CLASSES} -Xprof test.TestThreadSerial
