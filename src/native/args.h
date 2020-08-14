5d0e6feafd21bf9f04b23e04b2073a33f84fefde
alexvangogen <alexvangogen@gmail.com>
2020-08-14 12:18:05 +0300

    Refactor ParseOptions, make enum for options, use switch statement instead

---
+++ src/native/args.h
@@ -0,0 +1,49 @@
+#ifndef JCOZ_ARGS_H
+#define JCOZ_ARGS_H
+
+#include <string>
+#include <iostream>
+
+enum profiler_option
+{
+  _unknown,
+  _package,
+  _progress_point,
+  _end_to_end,
+  _warmup,
+  _fix_exp
+};
+
+namespace agent_args
+{
+  profiler_option from_string(std::string &option)
+  {
+    if (option == "pkg" || option == "package") return _package;
+    if (option == "progress-point") return _progress_point;
+    if (option == "end-to-end") return _end_to_end;
+    if (option == "warmup") return _warmup;
+    if (option == "fix-exp") return _fix_exp;
+
+    return _unknown;
+  }
+
+  void print_usage()
+  {
+    std::cout
+      << "usage: java -agentpath:<absolute_path_to_agent>="
+      << "pkg=<package_name>_"
+      << "progress-point=<class:line_no>_"
+      << "end-to-end (optional)_"
+      << "warmup=<warmup_time_ms> (optional - default 5000 ms)"
+      << std::endl;
+  }
+
+  void report_error(const char *message)
+  {
+    std::cerr << message << std::endl;
+    print_usage();
+    exit(1);
+  }
+} // namespace agent_args
+
+#endif //JCOZ_ARGS_H
