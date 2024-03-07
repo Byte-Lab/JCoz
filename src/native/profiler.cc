8a0da8de6fc3572cf3988c723bb9edf6b166c689
David Vernet <void@manifault.com>
2024-03-07 09:25:53 -0600

    Revert "Use 65536 instead of LONG_MAX as the last bci for location range for the last line number entry"
    Reverting due to LICENSE change. Only author we were unable to get written
    confirmation from.
    
    This reverts commit 8b96e517b8218acfb385be43b10d51864ada2b42.

--- src/native/profiler.cc
+++ src/native/profiler.cc
@@ -55,7 +55,6 @@ __thread JNIEnv * Accessors::env_;
 #define MIN_EXP_TIME 5000
 
 #define NUM_CALL_FRAMES 200
-#define MAX_BCI 65536
 
 typedef std::chrono::duration<int, std::milli> milliseconds_type;
 typedef std::chrono::duration<long, std::nano> nanoseconds_type;
@@ -431,7 +430,7 @@ Profiler::runAgentThread(jvmtiEnv *jvmti_env, JNIEnv *jni_env, void *args) {
           } else {
             location_ranges.push_back(
                 std::pair<jint, jint>(entries[i].start_location,
-                  MAX_BCI));
+                  LONG_MAX));
           }
         }
       }
